4967b19222144b2a28d47fb3edae9b6c01f11363
Rajesh-rdy <rajesh234@gmail.com>
2025-12-23 07:07:49 +0000

    first commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header'
+
+const Cart = () => (
+  <>
+    <Header />
+    <img
+      alt="cart"
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      className="products-image"
+    />
+  </>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import Header from '../Header'
+
+const Products = () => (
+  <>
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      className="products-image" alt="products"
+    />
+  </>
+)
+
+export default Products
